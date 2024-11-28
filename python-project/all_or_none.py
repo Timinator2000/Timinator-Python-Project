950c92bcc793ca4874f49911c930157780c7c350
Timinator <74882103+Timinator2000@users.noreply.github.com>
2024-11-27 19:51:36 -0700

    Delete python-project/all_or_none.py

--- python-project/all_or_none.py
+++
@@ -1,6 +0,0 @@
-def minimize_all_or_none_sets(all_or_none_groups):
-
-    # Combine any groups that can be combined and return
-    # a minimum number of groups.
-    
-    return all_or_none_groups
